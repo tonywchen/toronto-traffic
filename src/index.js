e8531a7544c062cc16297c579b1f89d2a67ef94f
Tony Wei-Han Chen <tonywh.chen@gmail.com>
2020-11-19 18:28:49 -0500

    initial commit; load mapbox api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import MapboxGl from 'mapbox-gl';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+MapboxGl.accessToken = 'pk.eyJ1IjoidG9ueXdjaGVuIiwiYSI6ImNraHBlODYwZDBjcTMyem54YWw5bm8yajAifQ.3j0gYt0zWIa_5rXk63grLQ';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
