1986e71a5e983c76a17cf5d80517c3eee1ec0a2e
Tony Wei-Han Chen <tonywh.chen@gmail.com>
2020-12-10 19:06:07 -0500

    auto generate missing paths in path update script

--- server/scripts/traffic/updatePathData.js
+++ server/scripts/traffic/updatePathData.js
@@ -17,6 +17,7 @@
 const _ = require('lodash');
 
 const mongoose = require('mongoose');
+const Subroute = require('../../models/nextbus/Subroute');
 const Stop = require('../../models/nextbus/Stop');
 const Path = require('../../models/traffic/Path');
 const PathStatus = require('../../models/traffic/PathStatus');
@@ -32,51 +33,63 @@ mongoose.connect('mongodb://localhost:27017/toronto-traffic', {
   useUnifiedTopology: true
 });
 
-const processDirectionsResponse = (response) => {
-  /* const responseLegs = response.data.routes[0].legs || [];
-  const transitStep = responseLegs[0].steps.find((step) => {
-    return step['travel_mode'] === 'TRANSIT'
+const populatePaths = async () => {
+  const paths = await Path.find().lean();
+  const pathMap = {};
+  paths.forEach(({ from, to, legs }) => {
+    if (legs) {
+      const identifier = `${from}-${to}`;
+      pathMap[identifier] = {
+        from,
+        to
+      };
+    }
   });
 
-  let polylineObj;
-  if (!transitStep) {
-    polylineObj = response.data.routes[0].overview_polyline;
-  } else {
-    polylineObj = transitStep.polyline;
+  const potentialPathMap = {};
+  const subroutes = await Subroute.find().lean();
+  subroutes.forEach((subroute) => {
+    subroute.stops.forEach((stop, stopIndex) => {
+      if (stopIndex >= subroute.stops.length - 1) {
+        return;
   }
 
-  if (!polylineObj) {
-    return {
-      success: false
-    };
-  } */
+      const nextStopIndex = stopIndex + 1;
+      const stopTag = stop.tag;
+      const nextStopTag = subroute.stops[nextStopIndex].tag;
 
-  const polylineObj = response.data.routes[0].overview_polyline;
-  const polylineValue = polylineObj.points;
-
-  const legs = polyline.decode(polylineValue);
-  if (!legs || legs.length === 0) {
-    return {
-      success: false
-    };
-  }
-
-  const transformedLegs = legs.map((leg) => {
-    // For coordinates, Google Maps uses [Lat, Lng] format whereas
-    // Mapbox uses [Lng, Lat] format. For now we are sticking with
-    // Mapbox's convention
-    return [leg[1], leg[0]];
+      const identifier = `${stopTag}-${nextStopTag}`;
+      potentialPathMap[identifier] = {
+        from: stopTag,
+        to: nextStopTag
+      }
+    });
   });
 
-  return {
-    success: true,
-    legs: transformedLegs,
-    polyline: polylineValue
-  };
+  const pathIdentifiers = Object.keys(pathMap);
+  const potentialPathIdentifiers = Object.keys(potentialPathMap);
+  const missingPathIdentifiers = _.difference(potentialPathIdentifiers, pathIdentifiers);
+  const unexpectedPaths = _.difference(pathIdentifiers, potentialPathIdentifiers);
+
+  for (const missingPathIdentifier of missingPathIdentifiers) {
+    const missingPath = potentialPathMap[missingPathIdentifier];
+    if (!missingPath) {
+      return;
+  }
+
+    await Path.findOneAndUpdate({
+      from: missingPath.from,
+      to: missingPath.to
+    }, {}, {
+      new: true,
+      upsert: true
+  });
+  }
+
+  console.log(`${missingPathIdentifiers.length} missing paths have been automatically generated`);
 };
 
-
-(async () => {
+const fetchDirections = async () => {
   const googleMapsClient = new GoogleMapsClient({});
 
   const stopMap = {};
@@ -127,4 +140,53 @@ const processDirectionsResponse = (response) => {
     counter++;
     console.log(`${counter} of ${paths.length} updated`);
   }
+}
+
+const processDirectionsResponse = (response) => {
+  /* const responseLegs = response.data.routes[0].legs || [];
+  const transitStep = responseLegs[0].steps.find((step) => {
+    return step['travel_mode'] === 'TRANSIT'
+  });
+
+  let polylineObj;
+  if (!transitStep) {
+    polylineObj = response.data.routes[0].overview_polyline;
+  } else {
+    polylineObj = transitStep.polyline;
+  }
+
+  if (!polylineObj) {
+    return {
+      success: false
+    };
+  } */
+
+  const polylineObj = response.data.routes[0].overview_polyline;
+  const polylineValue = polylineObj.points;
+
+  const legs = polyline.decode(polylineValue);
+  if (!legs || legs.length === 0) {
+    return {
+      success: false
+    };
+  }
+
+  const transformedLegs = legs.map((leg) => {
+    // For coordinates, Google Maps uses [Lat, Lng] format whereas
+    // Mapbox uses [Lng, Lat] format. For now we are sticking with
+    // Mapbox's convention
+    return [leg[1], leg[0]];
+  });
+
+  return {
+    success: true,
+    legs: transformedLegs,
+    polyline: polylineValue
+  };
+};
+
+
+(async () => {
+  await populatePaths();
+  await fetchDirections();
 })();
