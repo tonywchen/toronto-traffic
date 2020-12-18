407e5a506382116e6a912ce027224e20471f7be8
Tony Wei-Han Chen <tonywh.chen@gmail.com>
2020-12-18 00:28:23 -0500

    add datepicker and improve css compile perf

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectNextTime, selectPreviousTime } from '../../actions/timeline';
 import moment from 'moment-timezone';
 import _ from 'lodash';
+import DatePicker from "react-datepicker";
 
 import { DATE_FORMAT, DAY_FORMAT } from '../common/Util';
+import { selectNextTime, selectPreviousTime } from '../../actions/timeline';
 
 import { ReactComponent as PreviousIcon } from '../icons/previous.svg';
 import { ReactComponent as NextIcon } from '../icons/next.svg';
-
 import Timeline from './timeline/Timeline';
+
+import "react-datepicker/dist/react-datepicker.css";
 
 const SPEED = {
   FAST: {
@@ -37,6 +39,7 @@ const Dashboard = ({ onDayChanged }) => {
 
   const [isPaused, setPaused] = React.useState(true);
   const [animationSpeed, setAnimationSpeed] = React.useState(SPEED.NORMAL.value);
+  const [isDatePickerFocused, setDatePickerFocused] = React.useState(false);
 
   /**
    * Animation Control Functions
@@ -106,6 +109,16 @@ const Dashboard = ({ onDayChanged }) => {
       }
     }
   };
+  const handleDaySelect = (date) => {
+    stopAnimateTimeline();
+    if (date) {
+      const newTime = moment(date).valueOf();
+
+      if (onDayChanged) {
+        onDayChanged(newTime);
+      }
+    }
+  };
 
   /**
    * Render Helper Functions
@@ -118,22 +131,21 @@ const Dashboard = ({ onDayChanged }) => {
       <div className="dashboard-controls py-1 flex justify-center">
         <div className="dashboard__select flex justify-center space-x-2">
           <button
-            className="dashboard__timeline-toggle align-middle rounded-full bg-black bg-opacity-50 text-gray-500 hover-hover:hover:text-white px-2 py-2 hover-hover:hover:bg-blue-500 disabled:opacity-25"
+            className="dashboard__timeline-toggle align-middle w-10 h-10 rounded-full bg-black bg-opacity-50 text-gray-500 hover-hover:hover:text-white hover-hover:hover:bg-blue-500 disabled:opacity-25"
             onClick={() => { handleDayChange(-1) }}
             disabled={isBeforeFirst || !isPaused}>
-            <div className="dashboard__timeline-toggle-inner w-6 h-6">
+            <div className="dashboard__timeline-toggle-inner w-6 h-6 m-2">
               <PreviousIcon />
             </div>
           </button>
-          <div className="dashboard-detail text-center font-bold w-28 flex flex-col space-y-1">
-            <h4 className="text-white text-sm">{moment(timestamps[0]).format(DATE_FORMAT)}</h4>
-            <h6 className="text-gray-500 text-xs">{moment(timestamps[0]).format(DAY_FORMAT)}</h6>
+          <div className="dashboard-detail text-center font-bold w-28">
+            { renderDayPickerAndInput() }
           </div>
           <button
-            className="dashboard__timeline-toggle align-middle rounded-full bg-black bg-opacity-50 text-gray-500 hover-hover:hover:text-white px-2 py-2 hover-hover:hover:bg-blue-500 disabled:opacity-25 disabled:pointer-events-none"
+            className="dashboard__timeline-toggle align-middle w-10 h-10 rounded-full bg-black bg-opacity-50 text-gray-500 hover-hover:hover:text-white hover-hover:hover:bg-blue-500 disabled:opacity-25 disabled:pointer-events-none"
             onClick={() => { handleDayChange(1) }}
             disabled={isAfterToday || !isPaused}>
-            <div className="dashboard__timeline-toggle-inner w-6 h-6">
+            <div className="dashboard__timeline-toggle-inner w-6 h-6 m-2">
               <NextIcon />
             </div>
           </button>
@@ -142,11 +154,50 @@ const Dashboard = ({ onDayChanged }) => {
     );
   };
 
+  const DatePickerInput = ({ value, onClick }) => {
+    return (
+      <div className="px-2 rounded hover-hover:hover:bg-blue-500 cursor-pointer">
+        <h4 className="text-white text-sm" onClick={onClick}>
+          {moment(value).format(DATE_FORMAT)}
+        </h4>
+        <h6 className="text-gray-300 text-xs">{moment(timestamps[0]).format(DAY_FORMAT)}</h6>
+      </div>
+    );
+  };
+
+  const renderDayPickerAndInput = () => {
+    const popperModifiers = {
+      offset: {
+        enabled: true,
+        offset: "0px, 10px"
+      }
+    };
+
+    const props = {
+      selected: timestamps[0],
+      onSelect: handleDaySelect,
+      onChange: () => {},
+      popperClassName: 'dashboard-datepicker',
+      popperPlacement: 'bottom',
+      popperModifiers: popperModifiers,
+      minDate: moment(dataStatus.first).toDate(),
+      maxDate: new Date()
+    };
+
+    return (
+      <DatePicker
+        {...props}
+        customInput={<DatePickerInput />}
+      />
+    );
+  };
+
   /**
    * Render Function
    */
   return (
     <div className="dashboard absolute inset-x-0 bottom-0 py-10 px-2 lg:px-4 z-50 flex flex-col items-center space-y-2">
+
       <div className="bg-black bg-opacity-75 w-min rounded-lg py-2 px-4">
         {renderDateControls()}
       </div>
