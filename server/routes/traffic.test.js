9220c8b69098ed7a3a5b6629f1ac6d7affecc150
Tony Wei-Han Chen <tonywh.chen@gmail.com>
2020-12-30 19:11:05 -0500

    add tests for traffic route

---
+++ server/routes/traffic.test.js
@@ -0,0 +1,80 @@
+const mockSearchBetween = jest.fn(); // (() => Promise.resolve({}));
+jest.mock('../services/Traffic', () => {
+  return {
+    searchBetween: mockSearchBetween
+  }
+});
+
+const request = require('supertest');
+const app = require('../app');
+
+describe('Test the `/traffic` path', () => {
+  const successReturnValue = {results: []};
+
+  beforeEach(() => {
+    mockSearchBetween.mockReset();
+    mockSearchBetween.mockResolvedValue(successReturnValue);
+  });
+
+  test('It should respond to the GET method when no dates are specified', async () => {
+    const response = await request(app).get(`/traffic`);
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual(successReturnValue);
+  });
+
+  test('It should make appropriate service call when given valid dates', async () => {
+    const fromDate = '2020-12-15';
+    const toDate = '2020-12-16';
+    const params = `fromDate=${fromDate}&toDate=${toDate}`;
+
+    await request(app).get(`/traffic?${params}`);
+
+    expect(mockSearchBetween).toHaveBeenCalledTimes(1);
+    expect(mockSearchBetween).toHaveBeenCalledWith(fromDate, toDate);
+  });
+
+  test('It should relay error messages from the service call when given valid dates', async () => {
+    mockSearchBetween.mockReset();
+    mockSearchBetween.mockRejectedValue(new Error('Internal Error'));
+
+    const fromDate = '2020-12-15';
+    const toDate = '2020-12-16';
+    const params = `fromDate=${fromDate}&toDate=${toDate}`;
+
+    const response = await request(app).get(`/traffic?${params}`);
+
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({
+      error: 'Internal Error'
+    });
+  });
+
+  test('It should throw an error when given invalid dates', async () => {
+    const fromDate = '2020-12-155';
+    const toDate = '2020-12-166';
+    const params = `fromDate=${fromDate}&toDate=${toDate}`;
+
+    const response = await request(app).get(`/traffic?${params}`);
+
+    expect(mockSearchBetween).toHaveBeenCalledTimes(0);
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({
+      error: 'Invalid dates are given'
+    });
+  });
+
+  test('It should throw an error when given an invalid date range', async () => {
+    const fromDate = '2020-12-17';
+    const toDate = '2020-12-16';
+    const params = `fromDate=${fromDate}&toDate=${toDate}`;
+
+    const response = await request(app).get(`/traffic?${params}`);
+
+    expect(mockSearchBetween).toHaveBeenCalledTimes(0);
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toEqual({
+      error: '"fromDate" must not be larger than "toDate"'
+    });
+  });
+});
